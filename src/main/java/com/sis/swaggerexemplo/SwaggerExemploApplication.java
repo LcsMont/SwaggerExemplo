c7baca3f7430074d485cc8a63bbf671a9fb97068
Lucas Monteiro <lucasmmnm@gmail.com>
2024-12-26 20:33:02 -0300

    Setup & config

---
+++ src/main/java/com/sis/swaggerexemplo/SwaggerExemploApplication.java
@@ -0,0 +1,13 @@
+package com.sis.swaggerexemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerExemploApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerExemploApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sis/swaggerexemplo/SwaggerExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sis.swaggerexemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerExemploApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
