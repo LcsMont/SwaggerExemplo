6fe1a2230461aafd076336701d84a508dda6d9d7
Lucas Monteiro <lucasmmnm@gmail.com>
2024-12-26 22:00:24 -0300

    Operation config para cada método

--- src/main/java/com/sis/swaggerexemplo/controller/AtividadeController.java
+++ src/main/java/com/sis/swaggerexemplo/controller/AtividadeController.java
@@ -2,6 +2,7 @@ package com.sis.swaggerexemplo.controller;
 
 import com.sis.swaggerexemplo.model.Atividade;
 import com.sis.swaggerexemplo.repository.AtividadeRepository;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,11 +16,13 @@ public class AtividadeController {
     @Autowired
     private AtividadeRepository atividadeRepository;
 
+    @Operation(summary = "Listar", description = "Método que retorna todos os dados", tags = "Atividades")
     @GetMapping
     public List<Atividade> listar() {
         return atividadeRepository.findAll();
     }
 
+    @Operation(summary = "Listar por Id", description = "Método que retorna um registro", tags = "Atividades")
     @GetMapping("/{id}")
     public ResponseEntity<Atividade> listarPorId(@PathVariable int id) {
         return atividadeRepository.findById(id)
@@ -27,11 +30,13 @@ public class AtividadeController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Salvar", description = "Método que salva um registro", tags = "Atividades")
     @PostMapping
     public Atividade criar(@RequestBody Atividade atividade) {
         return atividadeRepository.save(atividade);
     }
 
+    @Operation(summary = "Alterar", description = "Método que altera um registro", tags = "Atividades")
     @PutMapping("/{id}")
     public ResponseEntity<Atividade> atualizar(@PathVariable int id, @RequestBody Atividade atividadeAtualizada) {
         return atividadeRepository.findById(id)
@@ -45,6 +50,7 @@ public class AtividadeController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Deletar", description = "Método que apaga um registro", tags = "Atividades")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletar(@PathVariable int id) {
         if (atividadeRepository.existsById(id)) {
